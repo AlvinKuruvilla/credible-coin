6e91f83c44d08811efbb020895a8efbb1f798a80
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-03-07 22:41:59 -0500

    First cut of some of the trait implementations
    There are no parameters or return types yet, but most of the main traits
    are in place and the directory structure is setup for now

---
+++ credible-coin/src/accumulator/mod.rs
@@ -0,0 +1,7 @@
+// FIXME: Add parameters and return types
+
+trait AbstractAccumulator {
+    fn prove_member();
+    fn verify();
+    fn search();
+}
---
+++ credible-coin/src/exchange/mod.rs
@@ -0,0 +1,11 @@
+// FIXME: Add parameters and return types
+trait AbstractExchange {
+    fn create_coin();
+    fn create_private_key();
+    fn create_random_number_generator();
+    fn create_coins();
+    fn push_to_db();
+    fn ad_coin_to_db();
+    fn update_coin_value();
+    fn get_coin();
+}
