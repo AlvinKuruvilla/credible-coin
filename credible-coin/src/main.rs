76c214da830918a574781d92a3d1dab2ac616ad0
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-03-30 01:33:09 -0400

    Get a series from a column given a column name

--- credible-coin/src/main.rs
+++ credible-coin/src/main.rs
@@ -1,5 +1,5 @@
 use credible_coin::utils;
 fn main() {
-    let series = utils::csv_reader::read_bitcoin_address_series();
+    let series = utils::csv_reader::get_dataset_column_by_name("addresses");
     println!("The current directory is {}", series);
 }
--- credible-coin/src/utils/csv_reader.rs
+++ credible-coin/src/utils/csv_reader.rs
@@ -1,9 +1,12 @@
 use polars::prelude::*;
-pub fn read_bitcoin_address_series() -> Series {
+pub fn read_bitcoin_address_dataframe() -> DataFrame {
     let df = CsvReader::from_path("BigQuery Bitcoin Historical Data - outputs.csv")
         .unwrap()
         .finish()
         .unwrap();
-    let addresses = df.column("addresses").unwrap().clone();
-    return addresses;
+    return df;
+}
+pub fn get_dataset_column_by_name(name: &str) -> Series {
+    let df = read_bitcoin_address_dataframe();
+    return df.column(name).unwrap().clone();
 }
