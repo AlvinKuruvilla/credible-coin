fef3d800d2ecdfe075df751207ddc4b0b8ddc40e
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-04-01 16:09:41 -0400

    Add bitcoin address generation and csv writing

--- credible-coin/src/main.rs
+++ credible-coin/src/main.rs
@@ -1,5 +1,5 @@
 use credible_coin::utils;
 fn main() {
-    let series = utils::csv_reader::get_dataset_column_by_name("addresses");
+    let series = utils::csv_utils::get_dataset_column_by_name("addresses");
     println!("The current directory is {}", series);
 }
