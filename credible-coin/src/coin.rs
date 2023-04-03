90b1def1ddbda3a7443fc34e9cdead2617370657
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-04-02 23:34:55 -0400

    Construct a coin from an address string and value
    Also remove the Default trait derivation as we want to enforce
    creation with a value and address

--- credible-coin/src/coin.rs
+++ credible-coin/src/coin.rs
@@ -1,19 +1,22 @@
 //! A simple wrapper type representing a u32 value
 
 use serde::{Deserialize, Serialize};
-#[derive(Debug, Clone, Default, Deserialize, Serialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Coin {
-    value: String,
     coin_address: String,
+    value: String,
 }
 impl Coin {
-    pub fn new(&self) -> Self {
-        return Self::default();
+    pub fn new(&self, coin_address: String, value: String) -> Self {
+        return Self {
+            coin_address,
+            value,
+        };
     }
-    pub fn coin_value(&self) -> String{
+    pub fn coin_value(&self) -> String {
         return self.value.clone();
     }
-    pub fn coin_address(&self) -> String{
+    pub fn coin_address(&self) -> String {
         return self.coin_address.clone();
     }
 }
