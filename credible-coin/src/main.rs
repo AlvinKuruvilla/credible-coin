55f44ac80532bf1b99c978be298d63c067c8b709
NDiaz5 <ndiaz5@pride.hofstra.edu>
2023-04-09 02:41:48 +0000

    test for input commits

--- credible-coin/src/main.rs
+++ credible-coin/src/main.rs
@@ -3,6 +3,6 @@ fn main() {
     // let series = utils::csv_utils::get_dataset_column_by_name("addresses");
     // println!("The series is {}", series);
 
-    let (v1, v2) = utils::csv_utils::addresses_and_values_as_vectors();
+    let (v1, v2) = utils::csv_utils::addresses_and_values_as_vectors("BigQuery Bitcoin Historical Data - outputs.csv");
     println!("V1 {:?}", v2);
 }
