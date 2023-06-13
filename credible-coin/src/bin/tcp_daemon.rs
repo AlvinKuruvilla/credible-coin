94c4e48a239b2d934634dbcbff9011c95d1ce80c
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-06-12 22:39:51 -0400

    Start a super basic tcp listener
    This will allow us to test and send commands to the publisher and verifier.
    The shell and the listener do not interact directly with each other,
    rather, the common functions will be moved to a separate file, which both
    the shell and listener can use distinctly.
    The listener will be responsible for its own argument parsing logic,
    much of which will most likely come from the shell anyway.

---
+++ credible-coin/src/bin/tcp_daemon.rs
@@ -0,0 +1,20 @@
+use std::{
+    io::Read,
+    net::{TcpListener, TcpStream},
+};
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
+
+pub fn main() {
+    let listener = TcpListener::bind("127.0.0.1:2300").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
