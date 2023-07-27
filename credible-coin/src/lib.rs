377b8dd44e2ea2d1b7a33fcb08ecc597386bac7c
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-07-26 23:18:42 -0400

    Fix clippy warnings in ToHashable trait

--- credible-coin/src/lib.rs
+++ credible-coin/src/lib.rs
@@ -1,5 +1,6 @@
 #![deny(clippy::implicit_return)]
 #![allow(clippy::needless_return)]
+#![warn(rustdoc::missing_crate_level_docs)]
 //!
 //! CredibleCoin is a zero-knowledge proof system to asses cryptocurrency exchange solvency.
 //! Currently the plan is to implement this system for Bitcoin, however, this crate should be built so
