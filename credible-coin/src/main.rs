2e46ae4a5fe179dee93d58c6c85f5479881c174d
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2023-09-14 10:05:35 -0400

    Create a MerkleTreeFile
    This struct holds the parsed data from a Merkle Tree
    file to quickly build the tree from

--- credible-coin/src/main.rs
+++ credible-coin/src/main.rs
@@ -1,5 +1,15 @@
-use credible_coin::utils::address_generator::generate_n_address_value_pairs;
+use credible_coin::utils::{
+    address_generator::generate_n_address_value_pairs, merkle_utils::MerkleTreeFile,
+};
+use rs_merkle::{algorithms::Sha256, Hasher};
 
 fn main() {
-    generate_n_address_value_pairs(1000000);
+    // generate_n_address_value_pairs(1000000);
+    let merkle_file = MerkleTreeFile::new("dump.txt");
+    let leaf_values = merkle_file.leaves;
+
+    let leaves: Vec<[u8; 32]> = leaf_values
+        .iter()
+        .map(|x| Sha256::hash(x.as_bytes()))
+        .collect();
 }
--- credible-coin/src/utils/mod.rs
+++ credible-coin/src/utils/mod.rs
@@ -1,6 +1,22 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
 pub mod address_generator;
 pub mod csv_utils;
-/// A set of helper functions to convert vector and slice types
+/// A helper trait to convert vector and slice types
 /// to Vec<&[u8]> to be hashed by the sha crate
 pub mod hashable;
 pub mod merkle_utils;
+/// Read the nth line from the file if it exists
+pub fn nth_line_from_file(filename: &str, n: usize) -> io::Result<Option<String>> {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let line = reader.lines().nth(n);
+
+    match line {
+        Some(Ok(line_content)) => Ok(Some(line_content)),
+        Some(Err(e)) => Err(e),
+        None => Ok(None),
+    }
+}
